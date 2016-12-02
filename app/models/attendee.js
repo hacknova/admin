f8a7adbcee920b6d7dbb5670c64741cc3deb2e1e
Jared Allard <jaredallard@outlook.com>
2016-12-02 15:44:30 -0800

    add application serializer converting allowing _ params in attributes

--- app/models/attendee.js
+++ app/models/attendee.js
@@ -1,10 +1,11 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  email:      DS.attr(),
-  first_name: DS.attr(),
-  last_name:  DS.attr(),
-  full_name:  Ember.computed("first_name", "last_name", function() {
+  email:     DS.attr('string'),
+  firstName: DS.attr('string'),
+  lastName:  DS.attr('string'),
+  fullName:  Ember.computed("first_name", "last_name", function() {
     return `${this.get('firstName')} ${this.get('lastName')}`;
   })
 });
